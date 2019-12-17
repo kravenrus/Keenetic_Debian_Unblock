4168893d5554a406414f76a66c0b3fd70f6b83db
kravenrus <47505285+kravenrus@users.noreply.github.com>
2019-12-17 09:40:54 +0400

    #2

--- README.md
+++ README.md
@@ -12,16 +12,23 @@ apt install mc xmlstarlet cron net-tools curl ipset iptables dnsutils dnsmasq to
 ```
 cat /dev/null > /etc/tor/torrc
 cat /dev/null > /etc/dnsmasq.conf
+cat /dev/null > /chroot-services.list
 
 curl --output /etc/tor/torrc  --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/etc/tor/torrc
 curl --output /etc/dnsmasq.conf  --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/etc/dnsmasq.conf
 curl --output /etc/unblock.txt  --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/etc/unblock.txt
 curl --output /etc/init.d/unblock --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/etc/init.d/unblock
+curl --output /etc/init.d/runnohup --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/etc/init.d/runnohup
 curl --output /bin/rtor.sh --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/bin/rtor.sh
-curl --output chroot-services.list --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/chroot-services.list
+curl --output /bin/tgbot.sh --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/bin/tgbot.sh
+curl --output /bin/JSON.sh --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/bin/JSON.sh
+curl --output /chroot-services.list --insecure https://raw.githubusercontent.com/kravenrus/Keenetic_Debian_Unblock/master/chroot-services.list
 
 chmod +x /etc/init.d/unblock
+chmod +x /etc/init.d/runnohup
 chmod +x /bin/rtor.sh
+chmod +x /bin/tgbot.sh
+chmod +x /bin/JSON.sh
 
 /etc/init.d/unblock start
 ```
---
+++ bin/tgbot.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#https://api.telegram.org/bot1010779316:AAGMtsLpv4xbWLps50k6ggUIZzy36_DLwgc/sendMessage?chat_id=262695591
+
+API_TOKEN='1010779316:AAGMtsLpv4xbWLps50k6ggUIZzy36_DLwgc' # Токен бота из @BotFather
+CHAT_ID='262695591' # ID чата с которым боту предстоит работать @userinfobot
+URL='https://api.telegram.org/bot'$API_TOKEN # Глобальная ссылка взаимодействия с ботом
+MSG_URL=$URL'/sendMessage?chat_id='$CHAT_ID # Ссылка для отправки сообщения ботом
+UPD_URL=$URL'/getUpdates?offset=' # Ссылка для получения сообщения ботом
+OFFSET=0 # Счетчик обработанных обновлений
+TIMEOUT='&timeout=10' # Интервал обработки обновлений
+
+MSG="<b>\[TGBOT\]</b> Данный Telegram Bot был запущен.%0A==================================%0AВозможно роутер был перезапущен, но это не точно."
+
+curl -m 600 --socks5 127.0.0.1:9050 -k -s -X POST "$MSG_URL&parse_mode=html&text=$MSG" &>/dev/null # Отправка сообщения ботом при запуске
+
+### Запуск бота, для управления Keenetic
+
+if [ "$1" == "start" ]; then
+
+while true; do {
+	RES=$(curl -m 600 --socks5 127.0.0.1:9050 -k -s -X POST $UPD_URL$OFFSET$TIMEOUT)
+
+	TARGET=$(echo $RES | JSON.sh | egrep '\["result",0,"message","chat","id"\]' | cut -f 2)
+	OFFSET=$(echo $RES | JSON.sh | egrep '\["result",0,"update_id"\]' | cut -f 2)
+	MESSAGE=$(echo $RES | JSON.sh -s | egrep '\["result",0,"message","text"\]' | cut -f 2 | cut -d '"' -f 2)
+	USER=$(echo $RES | JSON.sh | egrep '\["result",0,"message","from","username"\]' | cut -f 2 | cut -d '"' -f 2)
+
+	[ -z "$MESSAGE" ] || echo "[$(date +%Y.%m.%d_%H:%M:%S)][$USER] $MESSAGE" >> /var/log/Telegram_Bot.log
+
+	OFFSET=$((OFFSET+1))
+
+	if [ $OFFSET != 1 ]; then
+		case $MESSAGE in
+			'/start') MSG="tg_say.sh START LOL";; # Настройка действия/вывода для /start
+			'/uname') MSG="$(uname -a)";; # Настройка действия/вывода для /uname
+			
+			'/ip') MSG="<b>\[TGBOT\]</b> На данный момент%0A<i>IP адрес: $(ndmq -p 'show interface' -x | xmlstarlet sel -t -m '//interface[link="up"][state="up"][global="yes"][defaultgw="yes"]' -v 'address' -n)</i>";;
+			
+			'/update') ndmq -p "interface ISP ip dhcp client release" && sleep 10 &&
+			ndmq -p "interface ISP ip dhcp client release" && /etc/init.d/tor restart &&
+			MSG="<b>\[TGBOT\]</b> IP адрес успешно изменен!%0AСервис Tor был перезапущен.%0A<i>Теперь ваш IP: $(ndmq -p 'show interface' -x | xmlstarlet sel -t -m '//interface[link="up"][state="up"][global="yes"][defaultgw="yes"]' -v 'address' -n)</i>";;
+			
+			'/services') MSG="<b>\[TGBOT\]</b> Информация о доступе%0A==================================%0AНа роутере настроен DDNS, доступ к сервисам можно получить через DNS или IP адрес. Поскольку DDNS может быть не обновлен, доступ к сервисам окажется закрытым. В данной ситации предпочтительно подключение по IP адресу.%0A---------------------------------------------------------------------%0A/services_ip - список IP%0A/services_ddns - список DDNS";;
+			'/services_ip') IP="$(ndmq -p 'show interface' -x | xmlstarlet sel -t -m '//interface[link="up"][state="up"][global="yes"][defaultgw="yes"]' -v 'address' -n)" MSG="<b>\[TGBOT\]</b> Доступные сервисы:%0A==================================%0AFTP:%0Aftp://$IP%0A---------------------------------------------------------------------%0AApache:%0Ahttp://$IP%0A---------------------------------------------------------------------%0ADLNA:%0Ahttp://$IP:8200%0A---------------------------------------------------------------------%0ATransmission:%0Ahttp://$IP:9091";;
+			'/services_ddns') MSG="<b>\[TGBOT\]</b> Доступные сервисы:%0A==================================%0AFTP:%0Aftp://kravenrus.mykeenetic.net%0Aftp://kravenrus.ddns.net%0A---------------------------------------------------------------------%0AApache:%0Ahttp://kravenrus.mykeenetic.net%0Ahttp://kravenrus.ddns.net%0Ahttps://kravenrus.mykeenetic.net%0Ahttps://kravenrus.ddns.net%0A---------------------------------------------------------------------%0ADLNA:%0Ahttp://kravenrus.mykeenetic.net:8200%0Ahttp://kravenrus.ddns.net:8200%0A---------------------------------------------------------------------%0ATransmission:%0Ahttp://kravenrus.mykeenetic.net:9091%0Ahttp://kravenrus.ddns.net:9091";;
+			*) MSG="$MESSAGE";; # Настройка вывод полученного сообщения
+		esac
+		
+	curl -m 600 --socks5 127.0.0.1:9050 -k -s -X POST "$MSG_URL&parse_mode=html&text=$MSG" &>/dev/null # Отправка сообщения ботом
+	
+	fi
+
+} &>/dev/null; done
+
+else
+
+curl -m 600 --socks5 127.0.0.1:9050 -k -s -X POST "$MSG_URL&parse_mode=html&text=$@" &>/dev/null # Отправка сообщения ботом
+
+fi
